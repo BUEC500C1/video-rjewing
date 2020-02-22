7ffd5768b6f095f568a7ef1f840baa179e6e61f8
rjewing <ewingryan98@gmail.com>
2020-02-22 14:38:56 -0500

    Fixed function call type

--- config.py
+++ config.py
@@ -19,24 +19,32 @@ config_parser.read("./keys")
 
 
 def load_config(key, filename='keys'):
-    if os.exists(filename):
+    if os.path.exists(filename):
         return config_parser.get(filename, key)
     return None
 
-class Config():
-    TWITTER_API_KEY =  load_config('consumer_key') or load_secret_from_path('EC500_TWITTER_API_KEY') or os.getenv('EC500_TWITTER_API_KEY')
-    TWITTER_SECRET_KEY = load_config('consumer_secret') or load_secret_from_path('EC500_TWITTER_SECRET_KEY') or os.getenv('EC500_TWITTER_SECRET_KEY')
-    TWITTER_ACCESS_TOKEN = load_config('access_token') or load_secret_from_path('EC500_TWITTER_ACCESS_TOKEN') or os.getenv('EC500_TWITTER_ACCESS_TOKEN')
-    TWITTER_ACCESS_SECRET = load_config('access_secret') or load_secret_from_path('EC500_TWITTER_ACCESS_SECRET') or os.getenv('EC500_TWITTER_ACCESS_SECRET')
 
-    SECRET_KEY = load_secret_from_path('EC500_SECRET_KEY') or os.getenv('EC500_SECRET_KEY', 'dev_key')
+class Config():
+    TWITTER_API_KEY = load_config('consumer_key') or load_secret_from_path(
+        'EC500_TWITTER_API_KEY') or os.getenv('EC500_TWITTER_API_KEY')
+    TWITTER_SECRET_KEY = load_config('consumer_secret') or load_secret_from_path(
+        'EC500_TWITTER_SECRET_KEY') or os.getenv('EC500_TWITTER_SECRET_KEY')
+    TWITTER_ACCESS_TOKEN = load_config('access_token') or load_secret_from_path(
+        'EC500_TWITTER_ACCESS_TOKEN') or os.getenv('EC500_TWITTER_ACCESS_TOKEN')
+    TWITTER_ACCESS_SECRET = load_config('access_secret') or load_secret_from_path(
+        'EC500_TWITTER_ACCESS_SECRET') or os.getenv('EC500_TWITTER_ACCESS_SECRET')
+
+    SECRET_KEY = load_secret_from_path(
+        'EC500_SECRET_KEY') or os.getenv('EC500_SECRET_KEY', 'dev_key')
 
     # Set Host and Port
     API_IP = '0.0.0.0'
     API_PORT = 5000
     API_PUBLIC_IP = get('https://api.ipify.org').text
 
-    GMAIL_EMAIL = load_secret_from_path('EC500_GMAIL_EMAIL') or os.getenv('EC500_GMAIL_EMAIL')
-    GMAIL_PASSWORD = load_secret_from_path('EC500_GMAIL_PASSWORD') or os.getenv('EC500_GMAIL_PASSWORD')
+    GMAIL_EMAIL = load_secret_from_path(
+        'EC500_GMAIL_EMAIL') or os.getenv('EC500_GMAIL_EMAIL')
+    GMAIL_PASSWORD = load_secret_from_path(
+        'EC500_GMAIL_PASSWORD') or os.getenv('EC500_GMAIL_PASSWORD')
 
     NUM_WORKERS = 4
